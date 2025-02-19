703fd8ce920fdcabc3a7a7a1efc886cff15c6298
Smegy <uvojtiska@gmail.com>
2025-02-19 11:08:19 +0100

    1.0

---
+++ src/main/java/Smited/smitedEconomy/Money.java
@@ -0,0 +1,44 @@
+package Smited.smitedEconomy;
+
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class Money {
+
+
+    public static HashMap<UUID, Integer> cash = new HashMap<>();
+
+    public Money(int money) {
+    int MoneyInt = money;
+    }
+
+
+
+    public static Money SetMoney(int money, UUID player) {
+        cash.put(player, money);
+        return new Money(money);
+    }
+
+    public static Money AddMoney(int money, UUID player, int add) {
+        cash.replace(player, money+add);
+        return new Money(money+add);
+    }
+
+    public static Money SubMoney(int money, UUID player, int sub) {
+        cash.replace(player, money-sub);
+        return new Money(money-sub);
+    }
+
+    public static Money MultMoney(int money, UUID player, int mult) {
+        cash.replace(player, money*mult);
+        return new Money(money*mult);
+    }
+
+    public static Money DivMoney(int money, UUID player, int div) {
+        cash.replace(player, money/div);
+        return new Money(money/div);
+    }
+}
---
+++ src/main/java/Smited/smitedEconomy/SmitedEconomy.java
@@ -0,0 +1,17 @@
+package Smited.smitedEconomy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SmitedEconomy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
